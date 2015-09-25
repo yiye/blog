458b41425db498668b353271783809e5dc2fdc71
yiye <yiye.js@gmail.com>
2015-09-26 02:14:52 +0800

    add pic

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-    this.body = "hello xiaoxin";
+    this.body = '<html><body><img src="https://img.alicdn.com/imgextra/i4/751054539/TB2MMN6fFXXXXXdXpXXXXXXXXXX_!!751054539.jpg" alt="" /></body></html>';
+
 });
 
 app.listen(80);
