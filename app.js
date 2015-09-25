7f160b6ecb467702324c5468aa95e95001ecef63
yiye <yiye.js@gmail.com>
2015-09-26 02:28:15 +0800

    add pic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var koa = require('koa');
 var app = koa();
 
 app.use(function *(){
-    this.body = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>小馨</title></head><body><style type="text/css">html,body,img{width: 100%;height: 100%;}</style><img src="https://img.alicdn.com/imgextra/i4/751054539/TB2MMN6fFXXXXXdXpXXXXXXXXXX_!!751054539.jpg" alt="" /></body></html>';
+    this.body = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>小馨</title></head><body><style type="text/css">html,body,img{width: 100%;}</style><img src="https://img.alicdn.com/imgextra/i4/751054539/TB2MMN6fFXXXXXdXpXXXXXXXXXX_!!751054539.jpg" alt="" /></body></html>';
 
 });
 
